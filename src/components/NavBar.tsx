eb3f9e3a6bba74eb29ef7f554b123eb09402cf81
yang020501 <thaiduong020501@gmail.com>
2023-02-28 21:57:13 +0700

    Resolve Review

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -30,6 +30,32 @@ function HideOnScroll(props: any) {
 		</Slide>
 	);
 }
+const AvatarMenuPaper = {
+	elevation: 0,
+	sx: {
+		overflow: 'visible',
+		filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+		mt: 1.5,
+		'& .MuiAvatar-root': {
+			width: 32,
+			height: 32,
+			ml: -0.5,
+			mr: 1,
+		},
+		'&:before': {
+			content: '""',
+			display: 'block',
+			position: 'absolute',
+			top: 0,
+			right: 30,
+			width: 10,
+			height: 10,
+			bgcolor: 'background.paper',
+			transform: 'translateY(-50%) rotate(45deg)',
+			zIndex: 0,
+		},
+	},
+}
 
 export default function NavBar() {
 	const dispatch = useDispatch();
@@ -107,32 +133,7 @@ export default function NavBar() {
 							open={avatarMenuOpen}
 							onClose={handleClose}
 							onClick={handleClose}
-							PaperProps={{
-								elevation: 0,
-								sx: {
-									overflow: 'visible',
-									filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-									mt: 1.5,
-									'& .MuiAvatar-root': {
-										width: 32,
-										height: 32,
-										ml: -0.5,
-										mr: 1,
-									},
-									'&:before': {
-										content: '""',
-										display: 'block',
-										position: 'absolute',
-										top: 0,
-										right: 30,
-										width: 10,
-										height: 10,
-										bgcolor: 'background.paper',
-										transform: 'translateY(-50%) rotate(45deg)',
-										zIndex: 0,
-									},
-								},
-							}}
+							PaperProps={AvatarMenuPaper}
 							transformOrigin={{ horizontal: 'right', vertical: 'top' }}
 							anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
 						>
