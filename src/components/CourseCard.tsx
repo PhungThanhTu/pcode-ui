acde1e0de5c92bca7b75815bb6d3904ee651f9ad
yang020501 <thaiduong020501@gmail.com>
2023-03-12 22:10:19 +0700

    Update CourseCard.tsx

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -3,37 +3,64 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import IconButton from '@mui/material/IconButton';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import Avatar from '@mui/material/Avatar';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const CourseCard = () => {
-	return (
-		<Card sx={{ maxWidth: 345 }}>
-			<CardHeader
-				action={
-					<IconButton aria-label="settings">
-						<MoreVertIcon />
-					</IconButton>
-				}
-				title="OOP"
-				subheader="Nguyen Hoang Thai Duong"
-			/>
-			<CardContent>This is a content</CardContent>
-			<CardActions disableSpacing>
-				<IconButton aria-label="add to favorites">
-					<FavoriteIcon />
-				</IconButton>
-				<IconButton aria-label="share">
-					<ShareIcon />
-				</IconButton>
 
-				<ExpandMoreIcon />
-			</CardActions>
-		</Card>
-	);
+
+const AvatarSx = {
+    position: 'absolute',
+    top: "-20%",
+    right: '5%',
+    height: '75px',
+    width: '75px'
+}
+const CardSx = {
+    minWidth: 320,
+    border: 'solid 1px black'
+}
+const CardContentSx = { 
+    position: 'relative',
+    paddingTop: '50%' ,
+    borderTop: 'solid 1px black',
+    borderBottom: 'solid 1px black'
+}
+const CardActionSx = {
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+}
+const CourseCard = () => {
+    return (
+        <Card sx={CardSx}>
+            <CardHeader
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                    </IconButton>
+                }
+                
+                title="OOP"
+                subheader="Nguyen Hoang Thai Duong"
+            />
+            <CardContent sx={CardContentSx}>
+                <Avatar sx={AvatarSx} />
+            </CardContent>
+            <CardActions
+                disableSpacing
+                sx={CardActionSx}>
+                <IconButton >
+                    <ShareIcon />
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>
+
+            </CardActions>
+        </Card>
+    );
 };
 
 export default CourseCard;
