ba2718965d64545f7c7b4aac13fb16f1149080d0
yang020501 <thaiduong020501@gmail.com>
2023-03-19 19:17:39 +0700

    Course Update
    type definitions.
    api definitions.

---
+++ src/api/courseApi.ts
@@ -0,0 +1,20 @@
+import { AxiosResponse } from 'axios';
+import protectedApi from './protectedApi';
+import { Course, CreateCourse, CreateCourseResponse } from '@/types/course.type';
+
+const courseApi = {
+	getAllCourse: async () => {
+		const result: AxiosResponse<Course> = await protectedApi.get('/course');
+		return result;
+	},
+	createCourse: async (body: CreateCourse) => {
+		const result: AxiosResponse<CreateCourseResponse> = await protectedApi.post('/course', body);
+		return result;
+	},
+    renameCourse: async (body : Course) =>{
+        const result: any = await protectedApi.put(`/course/${body.id}`, body.title);
+		return result;
+    }
+};
+
+export default courseApi;
