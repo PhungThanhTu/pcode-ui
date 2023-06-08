4e55bf0573b7b8c19fef8403b021610fb376ca02
yang020501 <thaiduong020501@gmail.com>
2023-06-09 00:12:56 +0700

    Update AuthPageLayout.tsx

--- src/layouts/AuthPageLayout.tsx
+++ src/layouts/AuthPageLayout.tsx
@@ -7,7 +7,7 @@ interface PropsWithChildrenOnly {
 
 const AuthPageLayout: React.FunctionComponent<PropsWithChildrenOnly> = (props: PropsWithChildrenOnly) => {
 	const PageSx: SxProps = {
-		background: ` url(${'../src/assets/background2.png'})`,
+		background: ` url(${'http://smartourism.vn/uploads/images/service/hinh-dich-vu-1(1).jpg'})`,
 		backgroundColor: '#0000000f',
 		backgroundBlendMode: 'soft-light',
 		width: '100%',
